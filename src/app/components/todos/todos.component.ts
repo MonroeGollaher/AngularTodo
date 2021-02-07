b60cd6d0db464afbe8dc6450a5891a0eb1e54b1f
Monroe Gollaher <mgollaher92@gmail.com>
2021-02-06 23:06:01 -0700

    get request from api + delete / complete functions

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo'
+import { TodoService } from '../../services/todo.service'
 
 @Component({
   selector: 'app-todos',
@@ -7,30 +8,13 @@ import { Todo } from '../../models/Todo'
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
   todos: Todo[];
-  constructor() {
-  }
+  constructor(private todoService:TodoService) {}
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo one',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo two',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Todo three',
-        completed: false
-      }
-    ]
-
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos
+    });
   }
 
 }
